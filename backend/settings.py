5839feb96b90cc4b35a00d878a7132be2bc84f30
AlexanderSkySych <SkySych@yandex.ru>
2025-08-03 14:14:16 +0300

    add default secret

--- backend/settings.py
+++ backend/settings.py
@@ -5,6 +5,6 @@ class SettingsAuth(BaseSettings):
     URL_KEYCLOAK: str = "http://keycloak:8080"
     ID_CLIENT: str = "reports-api"
     NAME_REALM: str = "reports-realm"
-    SECRET: str = ""
+    SECRET: str = "oNwoLQdvJAvRcL89SydqCWCe5ry1jMgq"
 
 settings_auth = SettingsAuth()
