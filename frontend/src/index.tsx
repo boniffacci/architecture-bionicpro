51e94082f12bede07645dc0d1d8861f5a6d6fb05
Алексей Головизин <agolovizin@alfabank.ru>
2025-11-03 18:10:41 +0300

    И добавляем построчные комментарии

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,23 @@
+// Импортируем библиотеку React для создания компонентов
 import React from 'react';
+// Импортируем ReactDOM для рендеринга React-приложения в DOM (версия для React 18+)
 import ReactDOM from 'react-dom/client';
+// Импортируем глобальные стили приложения
 import './index.css';
+// Импортируем главный компонент приложения
 import App from './App';
 
+// Создаем корневой элемент React приложения
 const root = ReactDOM.createRoot(
+  // Находим HTML-элемент с id="root" и приводим его к типу HTMLElement
   document.getElementById('root') as HTMLElement
 );
 
+// Рендерим приложение в корневой элемент
 root.render(
+  // StrictMode - режим разработки для выявления потенциальных проблем
   <React.StrictMode>
+    {/* Главный компонент приложения */}
     <App />
   </React.StrictMode>
 );
