356a6769d791a5ffa3637b7b2dafe6271f6a0647
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-11-21 00:25:46 +0300

    task 2 init

---
+++ olap-db/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS report (
+    id UUID default generateUUIDv4(),
+    user_id UInt32,
+    user_name String,
+    user_email String,
+    user_age UInt32,
+    user_gender String,
+    user_country String,
+    user_address String,
+    user_phone String,
+    prosthesis_type String,
+    muscle_group String,
+    signal_frequency UInt32,
+    signal_duration UInt32,
+    signal_amplitude Decimal(5,2),
+    signal_time DateTime
+) ENGINE = MergeTree()
+ORDER BY (user_id, prosthesis_type, signal_time);
