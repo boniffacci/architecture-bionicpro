a906b7a89e30720f3ba7141b260cb4351b5b6d1f
Alexey Rigin <alexrigin@mail.ru>
2025-11-24 18:52:16 +0300

    task1 updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,10 +12,7 @@ const keycloakConfig: KeycloakConfig = {
 const keycloak = new Keycloak(keycloakConfig);
 
 const initOptions = {
-  // onLoad: 'check-sso', // или 'login-required'
-  // checkLoginIframe: false,
   pkceMethod: 'S256', // Включаем PKCE с методом S256
-  // enableLogging: true, // для отладки
 };
 
 const App: React.FC = () => {
